e87b3ed70ec7d2f06d4d97b58d712f4218c91a96
Debiprasad <debiprasadsahoo987@gmail.com>
2025-07-29 23:59:17 +0530

    started implementing dto in the project

--- src/main/java/com/mykart/project/payload/CategoryDTO.java
+++ src/main/java/com/mykart/project/payload/CategoryDTO.java
@@ -1,4 +1,13 @@
 package com.mykart.project.payload;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CategoryDTO {
+    private Long categoryId;
+    private String categoryName;
 }
--- src/main/java/com/mykart/project/payload/CategoryResponse.java
+++ src/main/java/com/mykart/project/payload/CategoryResponse.java
@@ -1,4 +1,15 @@
 package com.mykart.project.payload;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CategoryResponse {
+    private List<CategoryDTO> content;
 }
