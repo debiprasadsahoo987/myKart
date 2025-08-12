cb5e82e283ec4a675d01f78a846cd7cfb04f8459
Debiprasad <debiprasadsahoo987@gmail.com>
2025-08-12 23:27:01 +0530

    added delete item from cart functionality

--- src/main/java/com/mykart/project/repositories/CartItemRepository.java
+++ src/main/java/com/mykart/project/repositories/CartItemRepository.java
@@ -2,9 +2,14 @@ package com.mykart.project.repositories;
 
 import com.mykart.project.model.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     @Query("select ci from CartItem ci where ci.cart.id = ?1 and ci.product.id = ?2")
     CartItem findCartItemByProductIdAndCartId(Long cartId, Long productId);
+
+    @Modifying
+    @Query("DELETE FROM CartItem ci WHERE ci.cart.cartId = ?1 AND ci.product.productId = ?2")
+    void deleteCartItemByProductIdAndCartId(Long cartId, Long productId);
 }
--- src/main/java/com/mykart/project/repositories/CartRepository.java
+++ src/main/java/com/mykart/project/repositories/CartRepository.java
@@ -2,6 +2,7 @@ package com.mykart.project.repositories;
 
 import com.mykart.project.model.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
@@ -11,5 +12,5 @@ public interface CartRepository extends JpaRepository<Cart, Long> {
     @Query("select c from Cart c where c.user.email = ?1 and c.cartId = ?2")
     Cart findKartByEmailAndCartId(String emailId, Long cartId);
 
-    Long cartId(Long cartId);
+
 }
